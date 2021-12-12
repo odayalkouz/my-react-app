d2f2891f3401715e423b7d54330ad1c707af77b5
Uday Al Kouz <odai.alkouz.alone@gmail.com>
2021-12-12 10:32:02 +0200

    7

--- src/home.js
+++ src/home.js
@@ -2,14 +2,14 @@ import {useState,useEffect} from "react";
 import BlogList from "./Blog-List";
 import useFetch from "./useFetch";
 const Home=() =>{
-const {data:blogs,isloading,Erros}=useFetch("http://localhost:8000/blogs/");
+const {data:blogs,isloading,Erros}=useFetch("http://localhost:8000/blogs");
 
     return(
         <div className="home">
             {Erros && <div>{Erros}</div>}
             {isloading && <div className="loading-container" >{isloading}   .Loaging ....</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs List" />}
+            {blogs && <BlogList blogs={blogs} title="All Blogs List"/>}
         </div>
     );
 }
-export default Home;
+export default Home;@5
