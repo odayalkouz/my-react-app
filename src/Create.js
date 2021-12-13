7deea4af465fdc35880c606adbe1f267fe0b2279
Uday Al Kouz <odai.alkouz.alone@gmail.com>
2021-12-13 12:28:05 +0200

    15

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,4 @@
 import {useState ,useEffect} from "react";
-
 const Create=()=>{
 const [title,setTitle]=useState("");
 const [body,setBody]=useState("");
@@ -7,31 +6,30 @@ const [author,setAuthor]=useState("empty");
 
 const HandleSubmit=(e)=>{
     e.preventDefault();
+    const blog ={title,body,author};
+    console.log(blog);
+    fetch("http://localhost:8000/blogs",{
+        method:"POST",
+        headers:{'Content-Type': 'application/json'},
+        body:JSON.stringify(blog)
+    }).then(()=>{
+        console.log("add new oday");
+    })
 };
     return(
         <div className="create">
             <h1>Add New Blog</h1>
             <form onSubmit={HandleSubmit}>
                 <label>blog title:</label>
-                <input
-                    type="text"
-                    required
-                    value={title}
-                    onChange={(e)=>setTitle(e.target.value)}
-                />
+                <input type="text" required value={title} onChange={(e)=>setTitle(e.target.value)}/>
                 <label>blog Body:</label>
-                <textarea
-                    required
-                    value={body}
-                    onChange={(e)=>{setBody(e.target.value)}}
-                ></textarea>
+                <textarea required value={body} onChange={(e)=>{setBody(e.target.value)}}></textarea>
                 <label>blog author:</label>
                 <select value={author} onChange={(e)=>{setAuthor(e.target.value)}}>
-                    <option value="..........">..........</option>
-                    <option value="oday">oday</option>
+                    <option value="..........">..........</option><option value="oday">oday</option>
                     <option value="ahmad">ahmad</option>
                 </select>
-                <button >Add Blog</button>
+                <button>Add Blog</button>
                 <p>{title}</p>
                 <p>{body}</p>
                 <p>{author}</p>
@@ -39,5 +37,4 @@ const HandleSubmit=(e)=>{
         </div>
     )
 };
-
 export default Create;
--- src/home.js
+++ src/home.js
@@ -1,11 +1,11 @@
 import BlogList from "./Blog-List";
 import useFetch from "./useFetch";
 const Home=() =>{
-const {data:blogs,isloading,Erros}=useFetch("https://jsonplaceholder.typicode.com/posts");
+const {data:blogs,isloading,Erros}=useFetch("http://localhost:8000/blogs");
     return(
         <div className="home">
             {Erros && <div>{Erros}</div>}
-            {isloading && <div className="loading-container" >{isloading}   .Loaging ....</div>}
+            {isloading && <div className="loading-container" >{isloading}.... Loaging ....</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs List"/>}
         </div>
     );
