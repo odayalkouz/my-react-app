809d7fe0baf6d2de498f5c2bc581c86abcba763c
Uday Al Kouz <odai.alkouz.alone@gmail.com>
2021-12-13 10:25:13 +0200

    12

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,24 @@
 import{Link} from "react-router-dom";
+import {useState} from "react";
 
 const Navbar= () => {
+
+    // const handleClick=(menuItem)=> {
+    //     this.setState({ active: menuItem });
+    // };
+
+    const [active,setActive]=useState(" ");
+
+    // const HandleClick=()=>{
+    //     setActive("active");
+    // };
+
     return(
         <nav className="navbar">
             <h1>my react Blog</h1>
             <div>
-                <Link to="/home">Home</Link>
-                <Link to="/create"  style={{color:"#FFF",backgroundColor:"red",borderRadius:"8px"}}>New blog</Link>
+                <Link to="/home" id={active}>Home</Link>
+                <Link to="/create" style={{color:"#FFF",backgroundColor:"red",borderRadius:"8px"}}>New blog</Link>
             </div>
         </nav>
     );
